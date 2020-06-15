f0660811ea5f228577d98ba34a2fd64205b74447
Ben Elgar <me@benelgar.com>
2020-06-15 16:11:47 +0100

    Remove stderr redirect

--- create-cluster.sh
+++ create-cluster.sh
@@ -2,6 +2,6 @@
 
 set -xeu
 
-CLUSTER_ID=$(krucible create cluster --display-name "$CLUSTER_DISPLAY_NAME" --cluster-duration "$CLUSTER_DURATION" --configure-kubectl 2> /dev/null)
+CLUSTER_ID=$(krucible create cluster --display-name "$CLUSTER_DISPLAY_NAME" --cluster-duration "$CLUSTER_DURATION" --configure-kubectl)
 echo "::set-output name=cluster-id::$CLUSTER_ID"
 chmod +r "$HOME/.kube/config"
